37d06dbe7534b2fcf124f31abeecf3429037aa68
Kyle Rachau <kyle.rachau@us.qinetiq.com>
2024-09-29 10:51:58 -0400

    updating amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "quickerstopse76a8f61": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "quickerstops": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
